19a563432909e2b7fe5e5589264df133e3bb3957
unknown <mattch@seas.upenn.edu>
2015-07-22 01:01:08 -0400

    get rid of unused imports

--- app/src/main/java/com/mattchiaravalloti/commonactors/ListActivity.java
+++ app/src/main/java/com/mattchiaravalloti/commonactors/ListActivity.java
@@ -1,6 +1,5 @@
 package com.mattchiaravalloti.commonactors;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
@@ -8,23 +7,8 @@ import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.ScrollView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.Scanner;
 import java.util.Set;
 
 
